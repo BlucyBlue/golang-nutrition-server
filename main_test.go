8f65b7c00fc961ae93ca4adf9a6b6dddc2ca95dc
BlucyBlue <rastko.pavlovic@protonmail.com>
2024-01-08 14:12:07 +0100

    Adding tests for register user endpoint

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,4 +19,27 @@ func TestRootEndpoint(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.JSONEq(t, `{"message":"Server is up!"}`, w.Body.String())
+}
+
+func TestRegisterUser(t *testing.T) {
+	// Get the router with the registered routes
+	router := SetupRouter()
+
+	// Create a request body with user data
+	requestBody := bytes.NewBufferString(`{"username": "testuser", "email": "test@example.com", "password": "testpass"}`)
+
+	// Create a request to send to the above route
+	req, err := http.NewRequest(http.MethodPost, "/register", requestBody)
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// Record the response
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	// Assert
+	assert.Equal(t, http.StatusCreated, w.Code)
+	assert.Contains(t, w.Body.String(), "User registered successfully")
 }
