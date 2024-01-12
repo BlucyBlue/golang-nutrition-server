ae5b562e46873e82e8911278645f32c28aa2441d
BlucyBlue <rastko.pavlovic@protonmail.com>
2024-01-12 11:01:25 +0100

    Migrations for creating product table

---
+++ migrations/5_create_products_table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Products (
+                          ProductID SERIAL PRIMARY KEY,
+                          Name VARCHAR(255) NOT NULL,
+                          Category VARCHAR(255) NOT NULL
+);
